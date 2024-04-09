d2ae48deab505e0ba853902de8c513b5cad555a1
RafeM <138534376+voltro33@users.noreply.github.com>
2024-04-09 18:10:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@ function displayTeamList() {
 function editCardContent(card, teamID, i) {
     fetch(`https://corsproxy.io/?https://api.football-data.org/v2/teams/${teamID}`, {
         headers: {
-            'X-Auth-Token': 'be3a4a0da29649b49f4e2993959b7c28'
+            'X-Auth-Token': '3ffc8b7376ce456eba25599a38e36bea'
         }
     })
     .then(response => response.json())
